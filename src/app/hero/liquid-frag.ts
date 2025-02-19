b20f859f564016cd42f5d8a18ca86eb399107290
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-02-19 19:16:28 +0100

    -

--- src/app/hero/liquid-frag.ts
+++ src/app/hero/liquid-frag.ts
@@ -140,7 +140,8 @@ void main() {
     float thin_strip_2_width = cycle_width * thin_strip_2_ratio;
 
     // opacity = 1. - smoothstep(.9 - .5 * u_edgeBlur, 1. - .5 * u_edgeBlur, edge);
-    opacity = smoothstep(1., 1. - 1e-4 - u_edgeBlur, edge);
+    opacity = 1. - smoothstep(.9, 1., edge);
+    // opacity = smoothstep(1., 1. - 1e-4 - u_edgeBlur, edge);
     opacity *= get_img_frame_alpha(img_uv, 1e-4);
 
     float noise = snoise(vUv - t + vec2(.0001));
