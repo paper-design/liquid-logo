c04e1c210eb15cd28a547f5cd4d6bc2b7fd4cbff
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-02-19 19:00:54 +0100

    -

--- src/app/hero/liquid-frag.ts
+++ src/app/hero/liquid-frag.ts
@@ -169,10 +169,8 @@ void main() {
     dir *= (.5 + .5 * pow(uv.y, 2.));
 
     dir *= cycle_width;
-    dir = mod(dir, 1.);
 
     dir -= t;
-    dir = mod(dir, 1.);
 
     float refr_r = refr;
     refr_r += .03 * bulge * noise;
